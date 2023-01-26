16541c0d7e67a4b92c6d62ab5a500f21b4b9427d
sweetpea22 <cindychau24@me.com>
2023-01-26 18:29:58 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sketch from 'react-p5';
+import p5Types from 'p5';
+
+let x = 50;
+let y = 50;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.createCanvas(500, 500).parent(canvasParentRef);
+  };
+  const draw = (p5: p5Types) => {
+    p5.background(0);
+    p5.ellipse(x, y, 70, 70);
+    x++;
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
 }
 
 export default App;
