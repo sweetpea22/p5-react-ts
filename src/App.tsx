0a232031f08b6460831976bba611ad8b06bd17dd
sweetpea22 <cindychau24@me.com>
2023-02-21 10:12:37 -0500

    Rewrite with functions

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,17 @@ import Sketch from 'react-p5';
 import p5Types from 'p5';
 
 const App: React.FC = () => {
-  
   let canvas: HTMLCanvasElement = document.createElement('canvas');
 
   let ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
 
   const setup = (p5: p5Types, parentRef: Element) => {
     p5.createCanvas(500, 500).parent(parentRef);
-    p5.noStroke()
+    p5.noStroke();
     p5.colorMode('hsb', 360, 100, 100, 100);
-  }
+  };
   const draw = (p5: any) => {
     p5.background(230, 30, 23);
-    let gradient = ctx.createRadialGradient(
-      p5.mouseX - 100,
-      p5.mouseY - 100,
-      0,
-      p5.mouseX + 100,
-      p5.mouseY + 100,
-      500
-    );
-    gradient.addColorStop(0, '#ffff08');
-    gradient.addColorStop(1, '#ffff48');
-    ctx.fillStyle = gradient;
     p5.noStroke();
     p5.fill(190, 100, 100);
     p5.ellipse(p5.mouseX, p5.mouseY, 300);
@@ -40,7 +28,6 @@ const App: React.FC = () => {
       <Sketch setup={setup} draw={draw} />
     </div>
   );
-}
-
+};
 
 export default App;
